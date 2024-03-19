09eacb553ffe9e1da4cd74e2f908983291f090e7
Edveika <edvinasbureika@gmail.com>
2024-03-19 19:51:21 +0200

    Sprout options picker menu added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 // Current year for copyright in the footer
 function updateCopyrightYear() {
-  var copyrightElement = document.getElementById('copyright-year');
+  var copyrightElement = document.getElementById("copyright-year");
   var yearObj = new Date();
 
   copyrightElement.textContent = yearObj.getFullYear();
 }
 updateCopyrightYear();
+
+document
+  .getElementById("btn-mobile-nav")
+  .addEventListener("click", function () {});
+
+document
+  .getElementById("sprout-varieties")
+  .addEventListener("click", function () {
+    const sproutOptions = document.getElementById("sprout-var-window");
+
+    if (sproutOptions.classList.contains("hidden"))
+      sproutOptions.classList.remove("hidden");
+  });
+
+document
+  .getElementById("sprout-var-window-close")
+  .addEventListener("click", function () {
+    const sproutOptions = document.getElementById("sprout-var-window");
+
+    if (!sproutOptions.classList.contains("hidden"))
+      sproutOptions.classList.add("hidden");
+  });
