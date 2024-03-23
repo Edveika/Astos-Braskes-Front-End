2fd7373edc87292ff0ed0d271c26ed884f15dafb
Edveika <edvinasbureika@gmail.com>
2024-03-23 20:25:02 +0200

    Mobile navigation window

--- js/main.js
+++ js/main.js
@@ -9,7 +9,21 @@ updateCopyrightYear();
 
 document
   .getElementById("btn-mobile-nav")
-  .addEventListener("click", function () {});
+  .addEventListener("click", function () {
+    const container = document.getElementById("mobile-nav-container");
+
+    if (container.classList.contains("hidden"))
+      container.classList.remove("hidden");
+  });
+
+document
+  .getElementById("mobile-container-close")
+  .addEventListener("click", function () {
+    const container = document.getElementById("mobile-nav-container");
+
+    if (!container.classList.contains("hidden"))
+      container.classList.add("hidden");
+  });
 
 document
   .getElementById("sprout-varieties")
