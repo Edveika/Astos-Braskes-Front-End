0caefca2243868689de110a8a9361fc3de69b6ca
Edveika <edvinasbureika@gmail.com>
2024-04-06 14:49:08 +0300

    Sprouts early

---
+++ js/sprouts.js
@@ -0,0 +1,35 @@
+const buttons = document.querySelectorAll(".btn-sprout");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const sproutContent =
+      this.closest(".sprout-option").querySelector(".sprout-content");
+    const icon = this.querySelector("ion-icon");
+
+    if (sproutContent.classList.contains("hidden")) {
+      sproutContent.classList.remove("hidden");
+      icon.setAttribute("name", "chevron-up-outline");
+    } else {
+      sproutContent.classList.add("hidden");
+      icon.setAttribute("name", "chevron-down-outline");
+    }
+  });
+});
+
+const navs = document.querySelectorAll(".sprout-option--nav");
+
+navs.forEach((nav) => {
+  nav.addEventListener("click", function () {
+    const sproutContent =
+      this.closest(".sprout-option").querySelector(".sprout-content");
+    const icon = this.querySelector("ion-icon");
+
+    if (sproutContent.classList.contains("hidden")) {
+      sproutContent.classList.remove("hidden");
+      icon.setAttribute("name", "chevron-up-outline");
+    } else {
+      sproutContent.classList.add("hidden");
+      icon.setAttribute("name", "chevron-down-outline");
+    }
+  });
+});
