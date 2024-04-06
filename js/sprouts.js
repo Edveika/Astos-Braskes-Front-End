d3bb62d592f974f1d86e7e4a9b7e0f7668aade11
Edveika <edvinasbureika@gmail.com>
2024-04-06 15:45:44 +0300

    click on navbar removed

--- js/sprouts.js
+++ js/sprouts.js
@@ -1,35 +1,18 @@
 const navs = document.querySelectorAll(".sprout-option--nav");
 
 navs.forEach((nav) => {
-  const buttons = document.querySelectorAll(".btn-sprout");
+  const button = nav.querySelector(".btn-sprout");
+  const sproutContent = nav.nextElementSibling;
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const sproutContent =
-        this.closest(".sprout-option").querySelector(".sprout-content");
-      const icon = this.querySelector("ion-icon");
-
-      if (sproutContent.classList.contains("hidden")) {
-        sproutContent.classList.remove("hidden");
-        icon.setAttribute("name", "chevron-up-outline");
-      } else {
-        sproutContent.classList.add("hidden");
-        icon.setAttribute("name", "chevron-down-outline");
-      }
-    });
-  });
-
-  nav.addEventListener("click", function () {
-    const sproutContent =
-      this.closest(".sprout-option").querySelector(".sprout-content");
-    const icon = this.querySelector("ion-icon");
-
+  button.addEventListener("click", function () {
     if (sproutContent.classList.contains("hidden")) {
       sproutContent.classList.remove("hidden");
-      icon.setAttribute("name", "chevron-up-outline");
+      nav.querySelector("ion-icon").setAttribute("name", "chevron-up-outline");
     } else {
       sproutContent.classList.add("hidden");
-      icon.setAttribute("name", "chevron-down-outline");
+      nav
+        .querySelector("ion-icon")
+        .setAttribute("name", "chevron-down-outline");
     }
   });
 });
