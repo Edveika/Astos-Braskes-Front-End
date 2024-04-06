5cc59febd4f89d6a503a72d463774a4f7265a0ba
Edveika <edvinasbureika@gmail.com>
2024-04-06 14:58:52 +0300

    handle clicks on the sprout-option nav bar, responsive design

--- js/sprouts.js
+++ js/sprouts.js
@@ -1,24 +1,24 @@
-const buttons = document.querySelectorAll(".btn-sprout");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", function () {
-    const sproutContent =
-      this.closest(".sprout-option").querySelector(".sprout-content");
-    const icon = this.querySelector("ion-icon");
-
-    if (sproutContent.classList.contains("hidden")) {
-      sproutContent.classList.remove("hidden");
-      icon.setAttribute("name", "chevron-up-outline");
-    } else {
-      sproutContent.classList.add("hidden");
-      icon.setAttribute("name", "chevron-down-outline");
-    }
-  });
-});
-
 const navs = document.querySelectorAll(".sprout-option--nav");
 
 navs.forEach((nav) => {
+  const buttons = document.querySelectorAll(".btn-sprout");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const sproutContent =
+        this.closest(".sprout-option").querySelector(".sprout-content");
+      const icon = this.querySelector("ion-icon");
+
+      if (sproutContent.classList.contains("hidden")) {
+        sproutContent.classList.remove("hidden");
+        icon.setAttribute("name", "chevron-up-outline");
+      } else {
+        sproutContent.classList.add("hidden");
+        icon.setAttribute("name", "chevron-down-outline");
+      }
+    });
+  });
+
   nav.addEventListener("click", function () {
     const sproutContent =
       this.closest(".sprout-option").querySelector(".sprout-content");
